9022a202467e6d46aade25c3f02040b8db3b213b
laramartin <lmartin@babbel.com>
2018-07-13 15:50:27 +0200

    Create custom container

--- hello_rectangle/lib/main.dart
+++ hello_rectangle/lib/main.dart
@@ -1,40 +1,30 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hello Rectangle',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Rectangle'),
-        ),
-        body: HelloRectangle(),
-      ),
-    ),
-  );
-}
-
-class HelloRectangle extends StatelessWidget {
+class MyRectangle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 400.0,
-        width: 300.0,
-        child: Center(
-          child: Text(
-            'can you do it faster!',
-            style: TextStyle(fontSize: 40.0),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
+        child: Container(
+            color: Colors.greenAccent,
+            height: 400.0,
+            width: 300.0,
+            child: Center(
+              child: Text(
+                'Hello!',
+                style: TextStyle(fontSize: 20.0),),
+            )
+        )
     );
   }
+}
+
+void main() {
+  print('Hello!');
+
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text("hot reload!")),
+      body: MyRectangle(),
+    ),
+  ));
 }
