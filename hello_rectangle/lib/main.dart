e8c8337a5e22e168d4686fd657198e6514cdefc4
laramartin <lmartin@babbel.com>
2018-07-13 17:17:26 +0200

    Lara is Fluttering

--- hello_rectangle/lib/main.dart
+++ hello_rectangle/lib/main.dart
@@ -4,22 +4,28 @@ class ListRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Container(
-          child: Padding(padding: EdgeInsets.all(16.0),
-            child: InkWell(
-              child: Row(
-                children: <Widget>[
-                  new Padding(padding: EdgeInsets.all(10.0),
-                  child: FlutterLogo()
-                  ),
-                  new Center(
-                    child: Text(" is coming!"),
-                  )
-                ],
-              ),
-            ),
-          ),
+      color: Colors.greenAccent,
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            height: 100.0,
+            width: 300.0,
+            child: Padding(padding: EdgeInsets.all(16.0),
+                child: Row(
+                  children: <Widget>[
+                    new Center(
+                      child: Text("Lara is ", style: TextStyle(fontSize: 30.0)),
+                    ),
+                    new FlutterLogo(size: 50.0,
+                    ),
+                    new Center(
+                      child: Text("ing! 😱", style: TextStyle(fontSize: 30.0)),
+                    )
+                  ],
+            )
+          )
         )
+      )
     );
   }
 }
@@ -29,7 +35,7 @@ void main() {
 
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Text("List row")),
+      appBar: AppBar(title: Text("whaaaat")),
       body: ListRow(),
     ),
   ));
