d5c0b1cec298ec7adf82b369d0245b965118b3ea
laramartin <lmartin@babbel.com>
2018-08-05 17:42:04 +0200

    Added Input TextField

--- udacity-course/course/06_input/task_06_input/lib/converter_route.dart
+++ udacity-course/course/06_input/task_06_input/lib/converter_route.dart
@@ -72,29 +72,15 @@ class _ConverterRouteState extends State<ConverterRoute> {
 
     // TODO: Return the input, arrows, and output widgets, wrapped in
 
-    // TODO: Delete the below placeholder code
-    final unitWidgets = widget.units.map((Unit unit) {
-      return Container(
-        color: widget.color,
-        margin: EdgeInsets.all(8.0),
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              unit.name,
-              style: Theme.of(context).textTheme.headline,
-            ),
-            Text(
-              'Conversion: ${unit.conversion}',
-              style: Theme.of(context).textTheme.subhead,
-            ),
-          ],
-        ),
-      );
-    }).toList();
-
-    return ListView(
-      children: unitWidgets,
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'input', border: OutlineInputBorder()),
+          ),
+        ],
+      ),
     );
   }
 }
