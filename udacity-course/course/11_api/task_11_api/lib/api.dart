ae9ec2a4a11ac2af2b5cab3c467e1a2c70366da3
laramartin <lmartin@babbel.com>
2018-08-24 11:54:33 +0200

    Revert "Network request to fetch currencies" to do it again
    This reverts commit 1691a03b9b14b69fd6eef85d3ada2ce60fe3ad73.

--- udacity-course/course/11_api/task_11_api/lib/api.dart
+++ udacity-course/course/11_api/task_11_api/lib/api.dart
@@ -2,17 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//https://flutter.udacity.com/currency/converter?from=US%20Dollar&to=Bitcoin&amount=1
-
-import 'dart:async';
-import 'dart:convert';
-
-import 'dart:io';
-
-const apiCategory = {
-  'name': 'Currency',
-  'route': 'currency',
-};
+// TODO: Import relevant packages
 
 /// The REST API retrieves unit conversions for [Categories] that change.
 ///
@@ -23,62 +13,20 @@ const apiCategory = {
 ///   GET /currency: get a list of currencies
 ///   GET /currency/convert: get conversion from one currency amount to another
 class Api {
-  final String _url = "https://flutter.udacity.com";
 
-  final _httpClient = HttpClient();
+  // TODO: Add any relevant variables and helper functions
 
+  // TODO: Create getUnits()
   /// Gets all the units and conversion rates for a given category.
   ///
   /// The `category` parameter is the name of the [Category] from which to
   /// retrieve units. We pass this into the query parameter in the API call.
   ///
   /// Returns a list. Returns null on error.
-  Future<List> getUnits(String category) async {
-    final uri = Uri.https(_url, '/$category');
-    final jsonResponse = await _getJson(uri);
-    if (jsonResponse == null || jsonResponse['units'] == null) {
-      print('Error retrieving units.');
-      return null;
-    }
-    return jsonResponse['units'];
-  }
 
+  // TODO: Create convert()
   /// Given two units, converts from one to another.
   ///
   /// Returns a double, which is the converted amount. Returns null on error.
-  Future<double> convert(
-      String category, String amount, String fromUnit, String toUnit) async {
-    final uri = Uri.https(_url, '/$category/convert',
-        {'amount': amount, 'from': fromUnit, 'to': toUnit});
-    final jsonResponse = await _getJson(uri);
-    if (jsonResponse == null || jsonResponse['status'] == null) {
-      print('Error retrieving conversion.');
-      return null;
-    } else if (jsonResponse['status'] == 'error') {
-      print(jsonResponse['message']);
-      return null;
-    }
-    return jsonResponse['conversion'].toDouble();
-  }
 
-  /// Fetches and decodes a JSON object represented as a Dart [Map].
-  ///
-  /// Returns null if the API server is down, or the response is not JSON.
-  Future<Map<String, dynamic>> _getJson(Uri uri) async {
-    try {
-      final httpRequest = await _httpClient.getUrl(uri);
-      final httpResponse = await httpRequest.close();
-      if (httpResponse.statusCode != HttpStatus.OK) {
-        return null;
-      }
-      // The response is sent as a Stream of bytes that we need to convert to a
-      // `String`.
-      final responseBody = await httpResponse.transform(utf8.decoder).join();
-      // Finally, the string is parsed into a JSON object.
-      return json.decode(responseBody);
-    } on Exception catch (e) {
-      print('$e');
-      return null;
-    }
-  }
 }
--- udacity-course/course/11_api/task_11_api/lib/category_route.dart
+++ udacity-course/course/11_api/task_11_api/lib/category_route.dart
@@ -7,7 +7,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
-import 'api.dart';
+// TODO: Import necessary package
 import 'backdrop.dart';
 import 'category.dart';
 import 'category_tile.dart';
@@ -32,7 +32,6 @@ class CategoryRoute extends StatefulWidget {
 class _CategoryRouteState extends State<CategoryRoute> {
   Category _defaultCategory;
   Category _currentCategory;
-
   // Widgets are supposed to be deeply immutable objects. We can update and edit
   // _categories as we build our app, and when we pass it into a widget's
   // `children` property, we call .toList() on it.
@@ -93,7 +92,7 @@ class _CategoryRouteState extends State<CategoryRoute> {
     // We only want to load our data in once
     if (_categories.isEmpty) {
       await _retrieveLocalCategories();
-      await _retrieveApiCategory();
+      // TODO: Call _retrieveApiCategory() here
     }
   }
 
@@ -129,37 +128,9 @@ class _CategoryRouteState extends State<CategoryRoute> {
     });
   }
 
+  // TODO: Add the Currency Category retrieved from the API, to our _categories
   /// Retrieves a [Category] and its [Unit]s from an API on the web
-  Future<void> _retrieveApiCategory() async {
-    // Add a placeholder while we fetch the Currency category using the API
-    setState(() {
-      _categories.add(Category(
-        name: apiCategory['name'],
-        units: [],
-        color: _baseColors.last,
-        iconLocation: _icons.last,
-      ));
-    });
-    final api = Api();
-    final jsonUnits = await api.getUnits(apiCategory['route']);
-    // If the API errors out or we have no internet connection, this category
-    // remains in placeholder mode (disabled)
-    if (jsonUnits != null) {
-      final units = <Unit>[];
-      for (var unit in jsonUnits) {
-        units.add(Unit.fromJson(unit));
-      }
-      setState(() {
-        _categories.removeLast();
-        _categories.add(Category(
-          name: apiCategory['name'],
-          units: units,
-          color: _baseColors.last,
-          iconLocation: _icons.last,
-        ));
-      });
-    }
-  }
+  Future<void> _retrieveApiCategory() async {}
 
   /// Function to call when a [Category] is tapped.
   void _onCategoryTap(Category category) {
